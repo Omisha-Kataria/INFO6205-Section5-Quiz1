9a098d6ad0b98adb9be3abd5661dffb84a9cf3ac
Omisha Kataria <kataria.o@northeastern.edu>
2025-01-22 16:27:07 -0500

    Quiz 1 Solved

--- java/src/main/java/org/example/InsertionSort.java
+++ java/src/main/java/org/example/InsertionSort.java
@@ -9,8 +9,18 @@ public class InsertionSort {
     }
 
     private void insert(int i, Comparable[] a) {
-        //Inserts the 'Transition element' into its correct position in the sorted portion of the array.
-        //TODO: TO BE IMPLEMENTED
+        Comparable key = a[i];
+        int j = i - 1; //current element store position
+
+        // Shift elements to the right if they are greater than the key
+        while (j >= 0 && a[j].compareTo(key) > 0) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        // Place it in correct position
+        a[j + 1] = key;
+
     }
 
     private void swap(Object[] a, int i, int j) {
